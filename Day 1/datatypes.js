4356d640b2bfdb8f2b350171b1cd6929287efa53
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-01-12 10:05:40 +0700

    30 Days of Javascript Day 1

---
+++ Day 1/datatypes.js
@@ -0,0 +1,19 @@
+let boolean = true;
+let str = "a freaking string";
+let nll = null;
+let udf =  undefined;
+
+// Multiple lines
+let firstName = "Binh";
+let lastName = "Truong Vu";
+let marital = false;
+let country = "Vietnam";
+let age = 24;
+
+// Single line
+let firstName = "Binh", lastName = "Truong Vu", marital = false, country = "Vietnam", age = 24
+
+// Console log:
+let myAge = 24, yourAge = 30;
+console.log("I am ", myAge, " years old");
+console.log("You are ", yourAge, " years old");
---
+++ Day 1/variable.js
@@ -0,0 +1,4 @@
+console.log(typeof(boolean));
+console.log(typeof(str));
+console.log(typeof(nll));
+console.log(typeof(udf));
