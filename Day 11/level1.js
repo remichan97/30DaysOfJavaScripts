795b637d723bf8280b5617c9dc3fcd20a2e169da
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-01-25 10:23:41 +0700

    Level 1 Day 11

---
+++ Day 11/level1.js
@@ -0,0 +1,23 @@
+const constants = [2.72, 3.14, 9.81, 37, 100];
+const countries = ["Finland", "Estonia", "Sweden", "Denmark", "Norway"];
+const rectangle = {
+	width: 20,
+	height: 10,
+	area: 200,
+	perimeter: 60,
+};
+
+// Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
+
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);
+
+// Destructure and assign the elements of countries array to fin, est, sw, den, nor
+let [fin, est, sw, den, nor] = countries;
+
+console.log(fin, est, sw, den, nor);
+
+// Destructure the rectangle object by its properties or keys.
+let kry = {...rectangle};
+
+kry;
