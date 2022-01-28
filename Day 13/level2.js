3bea38d001c18854aa0bcb5c03e3df90e04a3b0e
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-01-28 17:19:02 +0700

    Level 2 Day 13

---
+++ Day 13/level2.js
@@ -0,0 +1,8 @@
+// 10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10);
+
+// Write a warning message using console.warn()
+console.warn('Nobody want to see this');
+
+// Write an error message using console.error()
+console.error('Whoops.. My bad..');
