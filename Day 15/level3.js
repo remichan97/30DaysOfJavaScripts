41d290dc09ac676c7a91430e6a8d352eeb450ed9
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-01-30 23:56:35 +0700

    Level 3 Day 15

---
+++ Day 15/level3.js
@@ -0,0 +1,63 @@
+// Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
+/*
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+console.log('Count:', statistics.count()) // 25
+console.log('Sum: ', statistics.sum()) // 744
+console.log('Min: ', statistics.min()) // 24
+console.log('Max: ', statistics.max()) // 38
+console.log('Range: ', statistics.range() // 14
+console.log('Mean: ', statistics.mean()) // 30
+console.log('Median: ',statistics.median()) // 29
+console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+console.log('Variance: ',statistics.var()) // 17.5
+console.log('Standard Deviation: ', statistics.std()) // 4.2
+console.log('Variance: ',statistics.var()) // 17.5
+console.log('Frequency Distribution: ',statistics.freqDist()) // [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+*/
+
+let ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26];
+
+class Statistics {
+	constructor(arr) {
+		this.arr = arr;
+	}
+
+	getArr() {
+		return this.arr;
+	}
+
+	setArr(arr) {
+		this.arr = arr;
+	};
+
+	count() {
+		return this.arr.length;
+	}
+
+	sum() {
+		let sum = 0;
+
+		this.arr.forEach(it => sum += parseInt(it));
+
+		return sum;
+	}
+
+	min() {
+		return this.arr.sort()[0];
+	}
+
+	max(){
+		return this.arr.sort()[this.arr.length - 1];
+	}
+}
+
+const stats = new Statistics(ages);
+
+console.log(stats.count());
+console.log(stats.sum());
+console.log(stats.min());
+console.log(stats.max());
+
+
+
