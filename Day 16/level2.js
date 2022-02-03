a75ce13c6ebe49e60da4de3c08d312202422cbfb
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-02-03 23:30:53 +0700

    Level 3 Day 16

--- Day 16/level2.js
+++ Day 16/level2.js
@@ -106,4 +106,4 @@ const txt = `{
 `;
 
 // Stringify the students object with only firstName, lastName and skills properties
-console.log(JSON.stringify(student,[firstName, lastName, skills],3));
+console.log(JSON.stringify(student, [firstName, lastName, skills], 3));
