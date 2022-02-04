c5dcb1a1fcebd7b8f52540f53f7bc74b5b9ce363
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-02-04 17:41:58 +0700

    Level 1 Day 17

---
+++ Day 17/level1.js
@@ -0,0 +1,7 @@
+// Store you first name, last name, age, country, city in your browser localStorage.
+localStorage.setItem('firstName', 'Binh');
+localStorage.setItem('lastName', 'Truong Vu');
+localStorage.setItem('age', '24');
+localStorage.setItem('country', 'Vietnam');
+localStorage.setItem('city', 'Hanoi');
+
