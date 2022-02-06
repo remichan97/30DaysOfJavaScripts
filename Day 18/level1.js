6dde84f067c36332bb947e838c44f5bb5e2a15aa
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-02-06 22:15:28 +0700

    Level 1 Day 18

---
+++ Day 18/level1.js
@@ -0,0 +1,8 @@
+const countriesAPI = "https://restcountries.com/v2/all";
+const catsAPI = "https://api.thecatapi.com/v1/breeds";
+
+// Read the countries API using fetch and print the name of country, capital, languages, population and area.
+fetch(countriesAPI)
+	.then((Response) => Response.json())
+	.then((data) => console.log(data))
+	.catch((Error) => console.log(Error));
