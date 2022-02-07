ad03165a075213c7b2b3bfc1bcf696b1e621c0e4
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-02-07 16:56:00 +0700

    Level 2 Day 19

--- Day 19/level2.js
+++ Day 19/level2.js
@@ -1,1 +1,23 @@
 // Create a closure which has three inner functions
+function funcOne(){
+	let count = 0;
+	function plusOne() {
+		count++;
+		return count;
+	}
+	function minusOne() {
+		count--;
+		return count;
+	}
+	function multiply(num) {
+		return count * num;
+	}
+	return {
+		plusOne:plusOne(),
+		minusOne:minusOne(),
+		multiply:multiply(8),
+	}
+}
+
+const func = funcOne();
+console.log(func);
