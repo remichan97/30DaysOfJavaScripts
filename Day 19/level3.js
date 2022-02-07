cf42ef83ca9f872b235a329ea442eae3976866c4
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-02-07 23:31:10 +0700

    Level 3 Day 19

--- Day 19/level3.js
+++ Day 19/level3.js
@@ -1,1 +1,39 @@
 // Create a personAccount out function. It has firstname, lastname, incomes, expenses inner variables. It has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance inner functions. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+function personAccount() {
+	let firstname = "Binh";
+	let lastName = "Truong Vu";
+	let incomes = [
+		{
+			names: "work",
+			income: 150,
+		},
+	];
+	let expenses = [
+		{
+			names: "food",
+			expense: 15,
+		},
+	];
+	function totalIncome() {
+		let sum = 0;
+		incomes.forEach((it) => (sum += parseInt(it.income)));
+		return sum;
+	}
+	function totalExpense() {
+		let sum = 0;
+		expenses.forEach((it) => (sum += parseInt(it.expense)));
+		return sum;
+	}
+	function addIncome(names, income) {
+		incomes.push({ names: names, income: income });
+	}
+	function addExpense(names, expense) {
+		incomes.push({ names: names, expense: expense });
+	}
+	return {
+		totalIncome: totalIncome(),
+		totalExpense: totalExpense(),
+		addIncome: addIncome("bonus", 50),
+		addExpense: addExpense("bills", 75),
+	};
+}
